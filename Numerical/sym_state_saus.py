52992e649a39de7e90b36901d486dde6b7b9ed15
NameIsFerran <ferran.v.kan@gmail.com>
2018-03-14 11:37:45 +0100

    commit pls

---
+++ Numerical/sym_state_saus.py
@@ -0,0 +1,23 @@
+"""
+Created on Wed Mar 14 10:09:50 2018
+
+@author: Ferran
+"""
+import numpy as np
+from math import *
+import scipy as sp
+import matplotlib.pyplot as plt
+from control.matlab import *
+
+#from Cit_par import *
+# parameter values
+m = 3.0
+b = 9.0
+k = 60.0
+# matrices
+A = np.matrix([[-b/m, -k/m], [1, 0]])
+B = np.matrix([[ 1/m], [ 0]])
+C = np.matrix([[ 0, 1]])
+D = np.matrix([[ 0]])
+# system
+sys = sp.StateSpace(A, B, C, D)
